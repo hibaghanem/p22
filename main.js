bdeb0c46c8c00a72c2a5f8d4084be684407f9535
hibaghanem <129052157+hibaghanem@users.noreply.github.com>
2024-04-27 09:48:19 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+ let btnn = document.getElementById('btn').addEventListener("click" , function () {
+    var inputValue = document.getElementById('inp').value ;
+    var url = "https://www.restaurant.com?content=" + inputValue ;
+    window.location.href=url;
+})
+let btnn1 = document.getElementById('btn1').addEventListener("click" , function () {
+    var inputValue = document.getElementById('inp').value ;
+    var url = "https://www.restaurant.com?content=" + inputValue ;
+    window.location.href=url;
+})
+let btnn2 = document.getElementById('btn2').addEventListener("click" , function () {
+    var inputValue = document.getElementById('inp').value ;
+    var url = "https://www.restaurant.com?content=" + inputValue ;
+    window.location.href=url;
+})
+let btnn3 = document.getElementById('btn3').addEventListener("click" , function () {
+    var inputValue = document.getElementById('inp').value ;
+    var url = "https://www.restaurant.com?content=" + inputValue ;
+    window.location.href=url;
+})
